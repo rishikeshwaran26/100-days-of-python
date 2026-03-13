2250d5297f0c3e6aef4f4fb3d134d21bf44e479d
rishikeshwaran26 <risikesh0715@gmail.com>
2026-03-13 18:32:15 +0530

    Day-2 finished

--- Day-02/main.py
+++ Day-02/main.py
@@ -1,1 +1,7 @@
+print("Welcome to the tip calculator \n")
+tot_bill=int(input("What was the total bill \n"))
+tip=int(input("how much tip would you like to give?10,12,15 \n"))
+split=int(input("How many people to split bill"))
+each_split=((tot_bill+tip)/split)
+print(f"The split for each person is {each_split}")
 
