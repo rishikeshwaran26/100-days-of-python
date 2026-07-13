e73edd4e5981d0c46e96215a2839847829febcdf
rishikeshwaran26 <rishikesh0715@gmail.com>
2026-07-13 07:12:40 +0530

    Day 18 Draw a line

--- Day-18/main.py
+++ Day-18/main.py
@@ -3,9 +3,11 @@ from turtle import *
 
 shape("turtle")
 color("red")
-for _ in range(4):
-    forward(100)
-    right(90)
+for _ in range(14):
+    forward(10)
+    penup()
+    forward(10)
+    pendown()
 
 
 
